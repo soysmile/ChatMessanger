4e92e3fed27836d0b00fb4c8dfac82bbd15c824c
George <soysmile@outlook.com>
2018-12-03 13:49:18 +0200

    Initial Commit

---
+++ ChatMesenger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatMesenger
+//
+//  Created by George Heints on 12/3/18.
+//  Copyright © 2018 George Heints. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
